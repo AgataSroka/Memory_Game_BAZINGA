c2335c79b9ed59e2167b3ed1f43ab95f628280c8
Agata Połetek <poletekagata@gmail.com>
2019-11-09 17:24:10 +0100

    home view in progress_2

--- bazinga/src/components/App/App.js
+++ bazinga/src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import '../../styles/App.scss';
-
+import number1 from '../../images/number-1.png';
 
 class App extends Component {
     render() {
@@ -12,34 +12,134 @@ class App extends Component {
                 <div className='container'>
                     <div className='settings'>
                         <div className='main_settings'>
-                        <div className='main_info'>
-                            Czas: <span> 100 </span>
-                        </div>
-                        <div className='main_info'>
-                            Ilość odsłon: <span> 0 </span>
-                        </div>
+                            <div className='main_info'>
+                                Time: <span> 100 </span>
+                            </div>
+                            <div className='main_info'>
+                                Flips: <span> 0 </span>
+                            </div>
                         </div>
                         <div className='cards'>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
-                            <div className='card'> </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <div className='card_front card_face'> </div>
+                                <div className='card_back card_face'>
+                                    <img className='card_show' src={number1}/>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
