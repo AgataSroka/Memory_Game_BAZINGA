a180cbe3336e2623b7bac6f63068c32d9fef42be
Agata Połetek <poletekagata@gmail.com>
2019-11-14 00:13:09 +0100

    colorful cards for test

--- bazinga/src/components/App/App.js
+++ bazinga/src/components/App/App.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 import '../../styles/App.scss';
-import number1 from '../../images/number-1.png';
 
 class App extends Component {
     render() {
         return (
-
             <div className="App">
                 <h1 className='jello-horizontal'> Bazinga </h1>
-
                 <div className='container'>
                     <div className='settings'>
                         <div className='main_settings'>
@@ -19,133 +16,34 @@ class App extends Component {
                                 Flips: <span> 0 </span>
                             </div>
                         </div>
-                        <div className='cards'>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                            <div className='card'>
-                                <div className='card_front card_face'> </div>
-                                <div className='card_back card_face'>
-                                    <img className='card_show' src={number1}/>
-                                </div>
-                            </div>
-                        </div>
+                        <section className='cards'>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </section>
                     </div>
                 </div>
             </div>
+
         )
-            ;
+
     }
 }
 
--- bazinga/src/index.js
+++ bazinga/src/index.js
@@ -1,30 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-var animateCards = function(e) {
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 
-    //reset animation
-    e.target.classList.remove('animate');
+const cardView = ['red', 'red', 'yellow', 'yellow', 'yellowgreen', 'yellowgreen', 'blue', 'blue', 'pink', 'pink',
+    'violet', 'violet', 'orange', 'orange', 'darkviolet', 'darkviolet', 'grey', 'grey', 'turquoise', 'turquoise'];
 
-    e.target.classList.add('animate');
-    setTimeout(function(){
-        e.target.classList.remove('animate');
-    },900);
+let cards = [...document.querySelectorAll('.cards > div')];
+
+console.log(cards);
+
+const startTime = new Date().getTime();
+
+let activeCard = '';
+const activeCards = [];
+
+const gameLength = cards.length / 2;
+let gameResult = 0;
+
+const clickCard = function () {
+    activeCard = this;
+    if (activeCard === activeCards[0])
+        return;
+    activeCard.classList.remove('hidden');
+
+    if (activeCards.length === 0) {
+        console.log('first card');
+        activeCards[0] = activeCard;
+
+    } else {
+        console.log('second card');
+        cards.forEach(card => card.removeEventListener('click', clickCard));
+        activeCards[1] = activeCard;
+
+        setTimeout(function () {
+            if (activeCards[0].className === activeCards[1].className) {
+                console.log('winner');
+                activeCards.forEach(card => card.classList.add('off'));
+                gameResult++;
+                cards = cards.filter(card => !card.classList.contains('off'));
+                if (gameResult === gameLength) {
+                    const endTime = new Date().getTime();
+                    const gameTime = (endTime - startTime) / 1000;
+                    alert(`Congratulations! Your time: ${gameTime} seconds `)
+                }
+            } else {
+                console.log('loser');
+                activeCards.forEach(card => card.classList.add('hidden'))
+            }
+            activeCard = '';
+            activeCards.length = 0;
+            cards.forEach(card => card.addEventListener('click', clickCard))
+        }, 1000)
+    }
 };
 
-var cards = document.getElementsByClassName("card");
+const start = function () {
+    cards.forEach(card => {
+        const position = Math.floor(Math.random() * cardView.length);
+        card.classList.add(cardView[position]);
+        cardView.splice(position, 1)
+    });
 
-for (var i = 0; i <cards.length; i++) {
-    cards[i].addEventListener('click', animateCards, false);
-}
+    setTimeout(function () {
+        cards.forEach(card => {
+            card.classList.add('hidden');
+            card.addEventListener('click', clickCard)
+        })
 
+    }, 3000)
+
+};
+start()
