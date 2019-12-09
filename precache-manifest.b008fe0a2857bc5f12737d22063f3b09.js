3340ff441ea09ef3f70718672e4329bf6c02f30c
Agata Połetek <poletekagata@gmail.com>
2019-12-09 12:53:19 +0100

    Updates

---
+++ precache-manifest.b008fe0a2857bc5f12737d22063f3b09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f9184edb8fb3563365d621afa6b193",
+    "url": "/Memory_Game_BAZINGA/index.html"
+  },
+  {
+    "revision": "7ad69baab829ae5be995",
+    "url": "/Memory_Game_BAZINGA/static/css/main.ec0e519f.chunk.css"
+  },
+  {
+    "revision": "519a539dd02771b7a789",
+    "url": "/Memory_Game_BAZINGA/static/js/2.95c59df5.chunk.js"
+  },
+  {
+    "revision": "7ad69baab829ae5be995",
+    "url": "/Memory_Game_BAZINGA/static/js/main.36d4653d.chunk.js"
+  },
+  {
+    "revision": "f0971d5b581531531f3e",
+    "url": "/Memory_Game_BAZINGA/static/js/runtime-main.1d72909e.js"
+  },
+  {
+    "revision": "3e3311533372644a74c7c619124216ac",
+    "url": "/Memory_Game_BAZINGA/static/media/bubble-chat.3e331153.png"
+  },
+  {
+    "revision": "d88f4146d236fc9d8df79233e9f15570",
+    "url": "/Memory_Game_BAZINGA/static/media/universe.d88f4146.jpg"
+  }
+]);
